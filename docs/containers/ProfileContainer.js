ae93bc94fe4d3eb6acffe0037159854e97fe8556
william chang <wwwy3y3@gmail.com>
2018-03-06 18:19:23 +0800

    container

---
+++ docs/containers/ProfileContainer.js
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+import Profile from '../components/Profile';
+
+const PROFILE_QUERY = gql`
+  query GetProfile {
+    profile {
+      name
+      cover
+      thumb
+      description
+    }
+  }
+`;
+
+const withProfile = graphql(PROFILE_QUERY, {
+  props: ({data}) => {
+    if (data.loading || data.error) {
+      return {name: "", cover: "", thumb: "", description: ""};
+    }
+
+    return data.profile;
+  }
+});
+
+export default withProfile(Profile);
---
+++ docs/containers/TodoListContainer.js
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+import TodoList from '../components/TodoList';
+
+const TODOS_QUERY = gql`
+  query GetTodos {
+    todos {
+      id
+      content
+    }
+  }
+`;
+
+const withTodos = graphql(TODOS_QUERY, {
+  props: ({data}) => {
+    if (data.loading || data.error) {
+      return {todos: []};
+    }
+
+    return {
+      todos: data.todos
+    };
+  }
+});
+
+export default withTodos(TodoList);
