3d5755e24e24b6ab3bf0f362f3f55a3cfa353f88
william chang <wwwy3y3@gmail.com>
2018-03-21 19:39:47 +0800

    refactor resolver

---
+++ src/rtdb/queryResolver.ts
@@ -0,0 +1,93 @@
+import { ExecInfo } from 'graphql-anywhere/lib/async';
+import { Resolver } from 'graphql-anywhere';
+import { database } from 'firebase';
+import * as has from 'lodash/has';
+import { ResolverRoot, ResolverContext } from './types';
+import { createQuery } from './utils';
+
+const snapshotToArray = (snapshot: database.DataSnapshot, typename?: string): ResolverRoot[] => {
+  const ret = [];
+  snapshot.forEach(childSnapshot => {
+    ret.push({
+      __snapshot: childSnapshot,
+      __typename: typename
+    });
+    return false;
+  });
+  return ret;
+}
+
+const queryResolver: Resolver = async (
+  fieldName: string,
+  root: ResolverRoot,
+  args: any,
+  context: ResolverContext,
+  info: ExecInfo,
+) => {
+  const { directives, isLeaf, resultKey } = info;
+  const { database, exportVal } = context;
+  let {__snapshot: currentSnapshot, rootSnapshot} = root;
+
+  if (isLeaf) {
+    // default return is null, to avoid missing field error
+    let leafReturn: any = null;
+
+    // dealing with different directives
+    if (has(directives, 'key')) {
+      // leaf with @key, e.g id: ID @key
+      leafReturn = currentSnapshot.key;
+    } else if (has(directives, 'val')) {
+      // leaf with @val
+      // most of times, used when parsing to array, the value is scalar
+      // and need to be extracted as a field
+      leafReturn = currentSnapshot.val();
+    } else {
+      // no directive, so simply return value with specified resultKey
+      leafReturn = currentSnapshot.child(resultKey).val();
+    }
+
+    // export variables
+    if (has(directives, 'export')) {
+      context.exportVal[resultKey] = leafReturn;
+    }
+
+    return leafReturn;
+  }
+
+  // Either rootSnapshot not provided, or nested @rtdbQuery
+  // we fetch the new one and replace currentSnapshot
+  // p.s: rootSnapshot will be provided at subscription
+  // because we need to listen to event change outside the resolver
+  if (!rootSnapshot && has(directives, 'rtdbQuery')) {
+    const query = createQuery({
+      database,
+      directives: directives.rtdbQuery,
+      exportVal,
+      snapshot: currentSnapshot
+    });
+    currentSnapshot = await query.once('value');
+  }
+
+  // if it's nested selectionSet, we return the child
+  if (!isLeaf && !has(directives, 'rtdbQuery')) {
+    return (has(directives, 'array'))
+      ? snapshotToArray(currentSnapshot.child(resultKey), null)
+      : {
+        __snapshot: currentSnapshot.child(resultKey)
+      }; 
+  }
+
+  // type could be defined in different directives, @rtdbQuery, @rtdbSub...
+  const type = context.findType(directives);
+
+  // firebase treat all data as object, even array
+  // so we need a hint using @array to know when to parse object to array
+  return (has(directives, 'array'))
+    ? snapshotToArray(currentSnapshot, type)
+    : {
+      __snapshot: currentSnapshot,
+      __typename: type
+    };
+};
+
+export default queryResolver;
