a0054bb0c3d217d0132f2121e02d9c5c5e24b691
william chang <wwwy3y3@gmail.com>
2018-03-26 12:56:23 +0800

    mutation with body and typename

--- src/rtdb/mutationResolver.ts
+++ src/rtdb/mutationResolver.ts
@@ -1,14 +1,15 @@
 import { ExecInfo } from 'graphql-anywhere/lib/async';
 import { Resolver } from 'graphql-anywhere';
 import * as has from 'lodash/has';
+import * as last from 'lodash/last';
 import { database } from 'firebase';
-import { ResolverContext, ResolverRoot } from './types';
+import { MutationResolverContext, MutationResolverRoot } from './types';
 
 const resolver: Resolver = async (
   fieldName: string,
-  root: ResolverRoot,
+  root: MutationResolverRoot,
   args: any,
-  context: ResolverContext,
+  context: MutationResolverContext,
   info: ExecInfo
 ) => {
   const { resultKey, directives, isLeaf } = info;
@@ -20,30 +21,55 @@ const resolver: Resolver = async (
     return has(directives, 'pushKey') ? snapshot.key : null;
   }
 
+  // slice key from ref
+  if (isLeaf && has(directives, 'key')) {
+    const { mutationRef } = context;
+    const key = last(mutationRef.split('/'));
+    return key || null;
+  }
+
+  // __typename
+  if (isLeaf && resultKey === '__typename') {
+    return root.__typename || null;
+  }
+
+  // @rtdbUpdate, @rtdbSet, we simply return the input to make apollo cache work
+  if (isLeaf && root && has(root, 'payload')) {
+    return (root.payload && root.payload[resultKey]) || null;
+  }
+
   // By convention GraphQL recommends mutations having a single argument named 'input'
   // https://dev-blog.apollodata.com/designing-graphql-mutations-e09de826ed97
   const payload: any = args && args.input;
 
   // deal with @rtdbUpdate, @rtdbSet, @rtdbRemove
   if (has(directives, 'rtdbUpdate')) {
-    const {ref} = directives.rtdbUpdate;
+    const {ref, type} = directives.rtdbUpdate;
+    context.mutationRef = ref;
     await database.ref(ref).update(payload);
+    return {payload, __typename: type};
   } else if (has(directives, 'rtdbSet')) {
-    const {ref} = directives.rtdbSet;
+    const {ref, type} = directives.rtdbSet;
+    context.mutationRef = ref;
     await database.ref(ref).set(payload);
+    return {payload, __typename: type};
   } else if (has(directives, 'rtdbRemove')) {
     const {ref} = directives.rtdbRemove;
+    context.mutationRef = ref;
     await database.ref(ref).remove();
+    return {payload: null};
   } else if (has(directives, 'rtdbPush')) {
-    const {ref} = directives.rtdbPush;
+    const {ref, type} = directives.rtdbPush;
+    context.mutationRef = ref;
     const newRef = database.ref(ref).push();
     await newRef.set(payload);
     return {
-      __snapshot: newRef
+      __snapshot: newRef,
+      __typename: type
     };
   }
 
-  return null;
+  return payload;
 }
 
 export default resolver;
--- src/rtdb/types.ts
+++ src/rtdb/types.ts
@@ -7,10 +7,20 @@ export interface ResolverContext {
   findType: (directives: {[key: string]: DirectiveArgs}) => string
 }
 
+export interface MutationResolverContext extends ResolverContext {
+  mutationRef?: string
+}
+
 export interface ResolverRoot {
   __typename?: string,
   __snapshot?: database.DataSnapshot,
   rootSnapshot?: database.DataSnapshot
+}
+
+export interface MutationResolverRoot {
+  __typename?: string,
+  __snapshot?: database.DataSnapshot,
+  payload?: any
 }
 
 export interface DirectiveArgs {
